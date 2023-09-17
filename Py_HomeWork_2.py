6bfff1ccec248aa43006b64506128a199900b26c
Lopatin Evgenii Alexandrovich <i@lopatin-ea.ru>
2023-09-18 00:34:49 +0700

    hw 4 create

--- Py_HomeWork_2.py
+++ Py_HomeWork_2.py
@@ -1,62 +1,58 @@
 # Задача 10:
-# На столе лежат n монеток. Некоторые из них лежат вверх
-# решкой, а некоторые – гербом. Определите минимальное число
-# монеток, которые нужно перевернуть, чтобы все монетки были
-# повернуты вверх одной и той же стороной. Выведите минимальное
+# На столе лежат n монеток. Некоторые из них лежат ввер решкой, а некоторые – гербом. Определите минимальное число
+# монеток, которые нужно перевернуть, чтобы все монетки былjповернуты вверх одной и той же стороной. Выведите минимальное
 # количество монет, которые нужно перевернуть.
-
 # 5 -> 1 0 1 1 0
 # 2
 
-# # 1. Решение через Count_________________
-# import random
+# 1. Решение через Count_________________
+import random
 
-# # Вводим количество монеток
-# n = int(input("Введите количетсво монеток: "))
-# print()
+# Вводим количество монеток
+n = int(input("Введите количетсво монеток: "))
+print()
+minimalResult = 0
 
-# minimalResult = 0
+# Заполняем строку случайными символами из набора
+coinSides = "OR"
+randomString = " ".join(random.choice(coinSides) for i in range(n))
 
-# # Заполняем строку случайными символами из набора
-# coinSides = "OR"
-# randomString = " ".join(random.choice(coinSides) for i in range(n))
+print("На столе лежит", n, "монеток в таком виде:", randomString)
+print("Орлов: ", n - randomString.count("R"), "Решек: ", randomString.count("R"))
 
-# print("На столе лежит", n, "монеток в таком виде:", randomString)
-# print("Орлов: ", n - randomString.count("R"), "Решек: ", randomString.count("R"))
+# Сравниваем количество орлов и решек
+if n - randomString.count("R") < randomString.count("R"):
+    minimalResult = n - randomString.count("R")
+else:
+    minimalResult = randomString.count("R")
 
-# # Сравниваем количество орлов и решек
-# if n - randomString.count("R") < randomString.count("R"):
-#     minimalResult = n - randomString.count("R")
-# else:
-#     minimalResult = randomString.count("R")
+print()
+print("Минимальное количество монет, которые нужно перевернуть: ", minimalResult)
+print()
 
-# print()
-# print("Минимальное количество монет, которые нужно перевернуть: ", minimalResult)
-# print()
+# 2. Решение через for_________________
 
-# # 2. Решение через for_________________
+import random
 
-# import random
+n = int(input("Введите количетсво монеток: "))
 
-# n = int(input("Введите количетсво монеток: "))
+minimalResult = 0
 
-# minimalResult = 0
+coinSides = [random.randint(0,1) for i in range(n)]
+print("На столе лежит", n, "монеток в таком виде:", coinSides)
 
-# coinSides = [random.randint(0,1) for i in range(n)]
-# print("На столе лежит", n, "монеток в таком виде:", coinSides)
+count = 0
+for side in coinSides:
+    if side == 1:
+        count += 1
 
-# count = 0
-# for side in coinSides:
-#     if side == 1:
-#         count += 1
+if n-count < count:
+    minimalResult = n-count
+else:
+    minimalResult = count
+print("Орлов: ", n-count, "Решек: ", count)
 
-# if n-count < count:
-#     minimalResult = n-count
-# else:
-#     minimalResult = count
-# print("Орлов: ", n-count, "Решек: ", count)
-
-# print("Минимальное количество монет, которые нужно перевернуть: ", minimalResult)
+print("Минимальное количество монет, которые нужно перевернуть: ", minimalResult)
 
 
 # Задача 12:
@@ -69,16 +65,16 @@
 # 4 4 -> 2 2
 # 5 6 -> 2 3
 
-# Решение________________
+# Решение______дискриминант__________
 
-# sum = int(input("Введите результат сложения чисел: "))
-# mul = int(input("Введите результат умноженич чисел: "))
+sum = int(input("Введите результат сложения чисел: "))
+mul = int(input("Введите результат умноженич чисел: "))
 
-# D = sum * sum - 4 * mul
-# X = (sum + (D ** (0.5)))/2
-# Y = (sum - (D ** (0.5)))/2
+D = sum * sum - 4 * mul
+X = (sum + (D ** (0.5)))/2
+Y = (sum - (D ** (0.5)))/2
 
-# print(X,Y)
+print(X,Y)
 
 
 # Задача 14:
@@ -87,13 +83,13 @@
 
 # 10 -> 1 2 4 8
 
-# # Решение 1_____________ответ в столбец_______________
+# Решение 1_____________ответ в столбец_______________
 
-# N = int(input('Введите число N: '))
-# i = 1
-# while i<=N:
-#     print(i)
-#     i*=2
+N = int(input('Введите число N: '))
+i = 1
+while i<=N:
+    print(i)
+    i*=2
 
 
 
