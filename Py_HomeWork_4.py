8eb684dd78db79bbf0d6a8fa8f3b6601890715bb
Lopatin Evgenii Alexandrovich <i@lopatin-ea.ru>
2023-09-19 00:16:28 +0700

    hw 5 create 1 task start

--- Py_HomeWork_4.py
+++ Py_HomeWork_4.py
@@ -5,30 +5,30 @@
 # Пользователь вводит 2 числа. n — кол-во элементов первого множества.
 # m — кол-во элементов второго множества. Затем пользователь вводит сами элементы множеств.
 
-# import random
+import random
 
-# n = int(input("Введите количесво элементов 1-го множества: "))
-# m = int(input("Введите количесво элементов 2-го множества: "))
-# print()
+n = int(input("Введите количесво элементов 1-го множества: "))
+m = int(input("Введите количесво элементов 2-го множества: "))
+print()
 
-# # list_1 = [random.randint(0, 9) for i in range(n)]
-# # list_2 = [random.randint(0, 9) for i in range(m)]
+# list_1 = [random.randint(0, 9) for i in range(n)]
+# list_2 = [random.randint(0, 9) for i in range(m)]
 
-# list_1 = [int(input(f"Введите {i+1} элемент 1-го множества: ")) for i in range(n)]
-# print()
-# list_2 = [int(input(f"Введите {i+1} элемент 2-го множества: ")) for i in range(n)]
-# print()
+list_1 = [int(input(f"Введите {i+1} элемент 1-го множества: ")) for i in range(n)]
+print()
+list_2 = [int(input(f"Введите {i+1} элемент 2-го множества: ")) for i in range(n)]
+print()
 
-# print("Множество 1: ", list_1)
-# print("Множество 2: ", list_2)
-# print()
+print("Множество 1: ", list_1)
+print("Множество 2: ", list_2)
+print()
 
-# result_list = []
-# for i in list_1:
-#     if i in list_2 and not i in result_list:
-#         result_list.append(i)
+result_list = []
+for i in list_1:
+    if i in list_2 and not i in result_list:
+        result_list.append(i)
 
-# print("Повторяющиеся числа в порядке возрастания: ", sorted(result_list))
+print("Повторяющиеся числа в порядке возрастания: ", sorted(result_list))
 
 
 # Задача 24: В фермерском хозяйстве в Карелии выращивают чернику. Она растёт на круглой грядке,
@@ -45,18 +45,18 @@
 # один заход собирающий модуль, находясь перед некоторым кустом заданной во входном файле грядки.
 
 
-# import random
+import random
 
-# n = int(input("Количество кустов ня грядке: : "))
-# garden_bed = [random.randint(0, 9) for i in range(n)]
+n = int(input("Количество кустов ня грядке: : "))
+garden_bed = [random.randint(0, 9) for i in range(n)]
 
-# print(garden_bed)
+print(garden_bed)
 
-# result_berries_sum = 0
+result_berries_sum = 0
 
-# for i in range(n):
-#     current_sum = garden_bed[i-1] + garden_bed[i] + garden_bed[(i+1)%n]
-#     if current_sum > result_berries_sum:
-#         result_berries_sum = current_sum
+for i in range(n):
+    current_sum = garden_bed[i-1] + garden_bed[i] + garden_bed[(i+1)%n]
+    if current_sum > result_berries_sum:
+        result_berries_sum = current_sum
 
-# print(result_berries_sum)
+print(result_berries_sum)
