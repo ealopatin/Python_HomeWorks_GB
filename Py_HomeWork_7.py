2b52a295b94da6797168cc48eff5cd712e44c3a8
Lopatin Evgenii Alexandrovich <i@lopatin-ea.ru>
2023-10-01 21:42:33 +0700

    start hw8.1

--- Py_HomeWork_7.py
+++ Py_HomeWork_7.py
@@ -14,13 +14,13 @@ def enter_adress_number():
     return input("Введите адрес: ").title()
 
 def enter_data():
-    name = enter_first_name()
     surname = enter_second_name()
+    name = enter_first_name()
     family = enter_family_name()
     phone_number = enter_phone_number()
     address = enter_adress_number()
     with open("boox.txt", "a", encoding="utf-8") as file:
-        file.write(f"{name} {surname} {family}\n{phone_number}\n{address}\n\n")
+        file.write(f"{surname} {name} {family}\n{phone_number}\n{address}\n\n")
 
 def print_data():
     with open("boox.txt", "r", encoding="utf-8") as file:
@@ -48,22 +48,24 @@ def search_line():
         # file.seek(0)
         # print([file.readlines()])
 
-def edit_data():
+# def edit_data():
+
+# def delete_data():
 
 def interface():
     cmd = 0
-    while cmd != "6":
+    while cmd != "0":
         print(
             "Варианты действия: \n"
             "1. Добавить контакт \n"
             "2. Вывести все контакты \n"
             "3. Поиск контакта \n"
-            "4. Редактировать контакт"
-            "5. Удалить контакт"
-            "6. Выход \n"
+            "4. Редактировать контакт\n"
+            "5. Удалить контакт\n"
+            "0. Выход \n"
         )
         cmd = input("Введите действие: ")
-        while cmd not in ("1", "2", "3", "4"):
+        while cmd not in ("1", "2", "3", "4","5","0"):
             print("Некорректный ввод")
             cmd = input("Введите действие: ")
         match cmd:
@@ -73,11 +75,11 @@ def interface():
                 print_data()
             case "3":
                 search_line()
-            case "4":
-                ....
-            case "5":
-                ....
-            case "6":
+            # case "4":
+                # edit_data()
+            # case "5":
+                # delete_data()
+            case "0":
                 print("Goodbuye!")
 
 # # search_line()
